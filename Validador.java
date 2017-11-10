ce6c63c3b684b1ccbe4b57b96704c0a22dc91da2
Marcos Sombra <markos.vs@live.com>
2017-11-10 06:28:48 -0300

    melhoria na validação de registradores

--- Validador.java
+++ Validador.java
@@ -16,7 +16,45 @@ public class Validador{
 	
 	final String[] pseudoInst = {"blt", "bgt", "ble", "bge", "li", "la"};
 
+	final String[] registradores = {"$zero", "at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1","$t2",
+		"$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9",
+		"$k0", "$k1", "$gp", "$sp", "fp", "$ra"};
+
+	final String[] registradoresNum = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","10", "11", "12", "13",
+		"14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "$27", "28", "29", "30",
+		"31"};
+
 	//métodos
+	//retorna o valor numerico do registrador
+	public int getNumReg(String reg){
+		for(int i = 0; i < 32; i++)
+			if(registradores[i].equals(reg) || registradoresNum[i].equals(reg))
+				return i;
+
+		return -1;
+	}
+
+	//checa se registrador existe
+	private boolean existeReg(int reg){
+		return reg >= 0 && reg <= 31;
+	}
+
+	private boolean existeReg(String registrador){
+		int reg = getNumReg(registrador);
+		return reg >= 0 && reg <= 31;
+	}
+
+	//checa se o programador pode alterar o registrador
+	public boolean regAlteravel(int reg){
+		return reg >= 2 && reg != 26 && reg != 27;
+	}
+
+	public boolean regAlteravel(String registrador){
+		int reg = getNumReg(registrador);
+		return reg >= 2 && reg != 26 && reg != 27;
+	}
+
+	//valida a instrução
 	public boolean eValida(String instrucao){
 		//checa se possui label e caso sim, ignora
 		if(instrucao.contains(":"))
@@ -30,6 +68,7 @@ public class Validador{
 
 		//variaveis para guardar os registradores
 		String r1, r2, r3;
+		int reg1, reg2, reg3;
 		//um loop pra cada array de tipos de instrução
 		//retorna verdadeiro caso a instrução seja reconhecida
 		//e falso caso não
@@ -40,10 +79,14 @@ public class Validador{
 		//identifica o 1º registrador e armazena
 		r1 = instrucao.split(" ")[1];
 		r1 = r1.substring(0, r1.indexOf(','));
+		reg1 = getNumReg(r1);
+		//se o registrador 1 não existir, retorna falso
+		if(!existeReg(r1))
+			return false;
 
 		for(String s : instTipoR){
-			//caso seja uma instrução que tenta salvar em $zero, retorna false
-			if(!s.equals("jr") && (r1.equals("$zero") || r1.equals("0")))
+			//caso seja uma instrução que tenta salvar em registradores reservados, retorna false
+			if(!s.equals("jr") && !regAlteravel(reg1))
 				return false;
 
 			if(inst.equals(s))
